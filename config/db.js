d35d0c5e5056739f2ab36ea52ec5a2f3f1b17758
antanikmitra <antanikmitra@hotmail.com>
2016-03-21 21:17:16 -0400

    File updated

---
+++ config/db.js
@@ -0,0 +1,12 @@
+// create public connection string
+module.exports = {
+
+    // local
+    // 'url': 'mongodb://localhost/test'
+
+    // live on mLab
+    /*'url': 'mongodb://gc200292856:200292856@ds064748.mlab.com:64748/gc200292856',
+    'githubClientId': 'a5612f2a55dd03a113f7',
+    'githubClientSecret': 'dfe3a6c43fd28a91992953a1f6b1e7a1f998a218',
+    'githubCallbackUrl': 'http://localhost:3000/auth/github/callback'*/
+};

7c5a65c8345f6cf48b22bf17a3626b8b79bc7e78
antanikmitra <antanikmitra@hotmail.com>
2016-03-25 23:02:30 -0400

    Changes according to assignment

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,31 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-      title: 'First Express Page',
-        message: 'This works pretty well so far'});
+      title: 'Welcome home Page',
+        message: 'Hi there'});
 });
 
-/* GET random page */
-router.get('/random', function(req, res, next) {
+
+/* GET Business page */
+router.get('/business', function(req, res, next) {
    
-    var ranNum = Math.random();
-    res.render('random', { title: 'Random',
-                          ranNum: ranNum });
+  
+  res.render('index', {
+      title: 'Business Page',
+        message: 'This is the business page'});
 });
+
+
+/* GET registration page */
+router.get('/registration', function(req, res, next) {
+   
+  
+  res.render('index', {
+      title: 'Registration Page',
+        message: 'This is the registration page'});
+});
+
+
 
 // make this public so the rest of app can see it
 module.exports = router;
